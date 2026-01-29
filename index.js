8a72e591d2cba60e034333165562f8ce2ada9e0d
Godson <gihemere@bountip>
2026-01-29 18:36:48 +0100

    added patch

--- index.js
+++ index.js
@@ -73,7 +73,7 @@ const upload = multer({
 app.use(cors({
   origin: ['http://localhost:5173', 'http://localhost:3000'],
   credentials: true,
-  methods: ['GET', 'POST', 'PUT', 'DELETE', 'OPTIONS'],
+  methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
 }));
 app.use(express.json());
