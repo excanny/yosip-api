2ebbbb6f9cf2025f8d32799cf9dc1534c4c46258
Godson <gihemere@bountip>
2026-01-29 19:37:13 +0100

    addd https://yosip.netlify.app/ to cors

--- index.js
+++ index.js
@@ -71,7 +71,7 @@ const upload = multer({
 
 // Middleware
 app.use(cors({
-  origin: ['http://localhost:5173', 'http://localhost:3000'],
+  origin: ['http://localhost:5173', 'http://localhost:3000', 'https://yosip.netlify.app'],
   credentials: true,
   methods: ['GET', 'POST', 'PUT', 'PATCH', 'DELETE', 'OPTIONS'],
   allowedHeaders: ['Content-Type', 'Authorization']
